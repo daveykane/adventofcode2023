c5fbdf1d47c400f1482af3c1512c1df8b6c3d27a
David Kane <david@osk.ltd>
2023-12-01 21:12:23 +0000

    Day One optimisation

--- src/day1/index.test.ts
+++ src/day1/index.test.ts
@@ -10,7 +10,7 @@ Rhum.testPlan("Advent of Code - Day One", () => {
       assertEquals(part1((await getInput("day1", "input-example1")).split("\n")), 142);
     });
     Rhum.testCase("should get 54605", async () => {
-      assertEquals(part1((await getInput("day1", "input")).split("\n")), 54605);
+      assertEquals(part1((await getInput("day1")).split("\n")), 54605);
     });
   });
 
@@ -18,9 +18,8 @@ Rhum.testPlan("Advent of Code - Day One", () => {
     Rhum.testCase("should get 281", async () => {
       assertEquals(part2((await getInput("day1", "input-example2")).split("\n")), 281);
     });
-
     Rhum.testCase("should get 55429", async () => {
-      assertEquals(part2((await getInput("day1", "input")).split("\n")), 55429);
+      assertEquals(part2((await getInput("day1")).split("\n")), 55429);
     });
   });
 });
--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,19 +1,12 @@
 const words = { one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 };
-const calibrationSum = (calibrations: string[], regex: RegExp) =>
-  calibrations.reduce((acc, curr) => {
-    let match;
-    const numbers = [];
-
-    while ((match = regex.exec(curr))) {
-      numbers.push(match[0]);
-      regex.lastIndex = match.index + 1;
-    }
-
+const calibrationSum = (calibrations: string[], regex: string) =>
+  calibrations.reduce((total, line) => {
+    const matches = Array.from(line.matchAll(new RegExp(`(?=(${regex}))`, "g")));
+    const numbers = matches.map((match) => match[1]);
     const first = (words as Record<string, number>)[numbers[0]] || numbers[0];
     const last = (words as Record<string, number>)[numbers[numbers.length - 1]] || numbers[numbers.length - 1];
-    return acc + parseInt(`${first}${last}`);
+    return total + parseInt(`${first}${last}`);
   }, 0);
 
-export const part1 = (calibrations: string[]) => calibrationSum(calibrations, /\d/g);
-export const part2 = (calibrations: string[]) =>
-  calibrationSum(calibrations, new RegExp(["\\d", ...Object.keys(words)].join("|"), "g"));
+export const part1 = (calibrations: string[]) => calibrationSum(calibrations, "\\d");
+export const part2 = (calibrations: string[]) => calibrationSum(calibrations, ["\\d", ...Object.keys(words)].join("|"));
